9d6ef86ccc93657cea6023c005d64e19a0de675e
Bruno Juwer <bruno.juwer1997@gmail.com>
2022-07-12 13:03:23 -0300

    Add comment feature

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -25,6 +25,18 @@ export function Post() {
           <a href="" target="_blank">#rocketseat</a> 
         </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea 
+          placeholder='Deixe um comentario'
+        />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
   )
 }
