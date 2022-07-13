8e7a924365dd8f436afda50f9a4b866343d05941
Bruno Juwer <bruno.juwer1997@gmail.com>
2022-07-13 16:16:22 -0300

    Add keys in the maps

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,8 +3,14 @@ import  ptBR  from 'date-fns/locale/pt-BR'
 import styles from './Post.module.css';
 import { Comment } from './Comment';
 import { Avatar } from './Avatar';
+import { useState } from 'react';
 
-export function Post({author, publishedAt, content}) {
+
+export function Post({id, author, publishedAt, content}) {
+
+  const [comments, setComments] = useState(['Post muito bacana, hein!'])
+  const [newComment, setNewComment] = useState('')
+
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
     locale:ptBR,
   })
@@ -13,6 +19,19 @@ export function Post({author, publishedAt, content}) {
     locale:ptBR,
     addSuffix: true,
   })
+
+  function hanldeCreateNewComment(e) {
+    e.preventDefault()
+
+    const newComment = e.target.commentArea.value
+    setComments([...comments, newComment])
+    setNewComment('')
+
+  }
+
+  function handleNewCommentChange() {
+    setNewComment(event.target.value); 
+  }
 
   return (
     <article className={styles.post}>
@@ -30,21 +49,24 @@ export function Post({author, publishedAt, content}) {
         </time>
       </header>
 
-      <div className={styles.content}>
-        {content.map(line => {
-          if(line.type === 'paragraph') {
-            return <p>{line.content}</p>
-          } else if(line.type === 'link') {
-            return <p><a href="#">{line.content}</a></p>
+       <div className={styles.content}>
+        {content.map(line =>{
+          if(line.type === 'paragraph'){
+            return <p key={line.content}>{line.content}</p>
+          } else if (line.type === 'link'){
+            return <p key={line.content}><a href="#">{line.content}</a></p>
           }
-        })}
-      </div>
+        })}        
+       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={hanldeCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
 
         <textarea 
-          placeholder='Deixe um comentario'
+          placeholder='Deixe um comentário'
+          name='commentArea'
+          value={newComment}
+          onChange={handleNewCommentChange}
         />
 
         <footer>
@@ -53,9 +75,9 @@ export function Post({author, publishedAt, content}) {
       </form>
 
       <dir className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map(comment => {
+          return <Comment key={comment} content={comment}/>
+        })}
       </dir>
     </article>
   )
